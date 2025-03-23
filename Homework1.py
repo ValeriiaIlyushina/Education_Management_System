bbed729f9c4cf24c801c6fce1b80e78cb2aeae61
Lerok <valeriaa129@mail.ru>
2025-03-23 20:27:05 +0300

    Homework3

--- Homework1.py
+++ Homework1.py
@@ -8,6 +8,14 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def mean_g(self):
+        if len(self.grades) > 0:
+            all_g=[n for i in self.grades.values() for n in i]
+            mean = sum(all_g)/len(all_g)
+            return mean
+        else:
+            return "оценок пока нет"
+
     def rate_hw(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in mentor.courses_attached:
             if course in lecturer.grades:
@@ -16,6 +24,28 @@ class Student:
                 lecturer.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {self.mean_g()}' \
+               f'\nКурсы в процессе изучения: {", ".join(self.courses_in_progress)}\nЗаверщённые курсы: {", ".join(self.finished_courses)}'
+
+    def __eq__(self, other):
+        return self.mean_g() == other.mean_g()
+
+    def __lt__(self, other):
+        return self.mean_g() < other.mean_g()
+
+    def __ne__(self, other):
+        return self.mean_g() != other.mean_g()
+
+    def __le__(self, other):
+        return self.mean_g() <= other.mean_g()
+
+    def __gt__(self, other):
+        return self.mean_g() > other.mean_g()
+
+    def __ge__(self, other):
+        return self.mean_g() >= other.mean_g()
 
 class Mentor:
 
@@ -29,6 +59,36 @@ class Lecturer(Mentor):
 
     def __init__(self, name, surname,):
         super().__init__(name,surname)
+        self.grades = {}
+
+    def mean_g(self):
+        if len(self.grades) > 0:
+            all_g = [n for i in self.grades.values() for n in i]
+            mean = sum(all_g) / len(all_g)
+            return mean
+        else:
+            return "Оценок пока нет"
+
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции:{self.mean_g()}'
+
+    def __eq__(self, other):
+        return self.mean_g() == other.mean_g()
+
+    def __lt__(self, other):
+        return self.mean_g() < other.mean_g()
+
+    def __ne__(self, other):
+        return self.mean_g() != other.mean_g()
+
+    def __le__(self, other):
+        return self.mean_g() <= other.mean_g()
+
+    def __gt__(self, other):
+        return self.mean_g() > other.mean_g()
+
+    def __ge__(self, other):
+        return self.mean_g() >= other.mean_g()
 
 
 class Reviewer(Mentor):
@@ -44,3 +104,6 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}'
