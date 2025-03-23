a8c4aec3f7893c93872b4e4a33efc13116ad9434
Lerok <valeriaa129@mail.ru>
2025-03-23 20:25:26 +0300

    Homework2

--- Homework1.py
+++ Homework1.py
@@ -8,6 +8,14 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_hw(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in mentor.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 class Mentor:
 
@@ -27,3 +35,12 @@ class Reviewer(Mentor):
 
     def __init__(self, name, surname,):
         super().__init__(name,surname)
+
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
